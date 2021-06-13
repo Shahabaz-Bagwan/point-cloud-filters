f18f710c3e9198b66606fd134d70ceec26752b9e
Shahabaz Bagwan <cool.shahabaz@gmail.com>
2021-06-13 20:20:32 +0530

    creates cmake project

---
+++ include/KdtreeFlann.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <flann/algorithms/dist.h>
+#include <flann/flann.h>
+#include <vector>
+
+using FLANNIndex = ::flann::Index<::flann::L2_Simple<double>>;
+
+struct Point {
+  double x, y, z;
+};
+
+using pointCloud = std::vector<Point>;
+
+class KDtreeFlann {
+public:
+  KDtreeFlann(pointCloud _input);
+
+  KDtreeFlann();
+
+  ~KDtreeFlann();
+
+  int knnSearch(Point _point, size_t _k, std::vector<size_t> &pointIdxKSearch,
+                std::vector<double> &pointKSquaredDistance);
+
+  void setInputCloud(pointCloud _input);
+
+  int radiusSearch(Point _point, double _radius,
+                   std::vector<size_t> &indices_radius,
+                   std::vector<double> &dists_radius);
+
+private:
+  std::vector<double> input, queryPt;
+
+  size_t dim = 3, original_points;
+
+  FLANNIndex *flann_index_;
+
+  // Parameter for searching
+  ::flann::SearchParams searchParam_;
+};
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <IterativeClosestPoint.hpp>
+#include <KdtreeFlann.hpp>
+#include <PointCloudFilters.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[]) { return 0; }
