9210bae75c71e63f967060d58638a0602187053b
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2024-01-26 12:16:33 +0100

    update point definition
    add all the required special member functions

--- include/pointCloudFilters/PointDefinition.hpp
+++ include/pointCloudFilters/PointDefinition.hpp
@@ -5,12 +5,30 @@ namespace PCF {
   struct Point
   {
     double x, y, z;
+    Point()                          = default;
+    ~Point()                         = default;
+    Point& operator=( const Point& ) = default;
+    Point& operator=( Point&& )      = default;
+    Point( const Point& p )          = default;
+    Point( Point&& p ) noexcept      = default;
+
+    Point( double x, double y, double z ) : x( x ), y( y ), z( z ) {}
   };
 
   using pointCloud = std::vector< Point >;
 
-  struct normalsAndCurvature
+  struct NormalsAndCurvature
   {
     double nx, ny, nz, curvature;
+    NormalsAndCurvature( double nx, double ny, double nz, double curvature )
+      : nx( nx ), ny( ny ), nz( nz ), curvature( curvature )
+    {
+    }
+    NormalsAndCurvature()                                        = default;
+    NormalsAndCurvature( const NormalsAndCurvature& )            = default;
+    NormalsAndCurvature( NormalsAndCurvature&& )                 = default;
+    NormalsAndCurvature& operator=( NormalsAndCurvature&& )      = default;
+    ~NormalsAndCurvature()                                       = default;
+    NormalsAndCurvature& operator=( const NormalsAndCurvature& ) = default;
   };
 } // namespace PCF
