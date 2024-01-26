61f902778afe5bc647e293131cbfb261c00a8a06
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2024-01-26 13:33:59 +0100

    calculate time required for align call

--- example/ipc/src/main.cpp
+++ example/ipc/src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <pointCloudFilters/FileHandler.hpp>
 #include <pointCloudFilters/IterativeClosestPoint.hpp>
@@ -18,13 +19,18 @@ int main( int argc, char* argv[] )
 
   IterativeClosestPoint ipc;
   pointCloud output;
+  auto start = std::chrono::high_resolution_clock::now();
   ipc.align( 100, 20, A, B, output );
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration =
+    std::chrono::duration_cast< std::chrono::microseconds >( stop - start );
+  std::cout << "time: " << duration.count() << "µs" << std::endl;
+
+  // for( const auto p : output ) {
+  //   printf( "x:%f, y:%f\n", p.x, p.y );
+  // }
 
   auto tx = ipc.getTxMatrix();
-
-  for( const auto p : output )
-    printf( "x:%f, y:%f\n", p.x, p.y );
-
   std::cout << tx << std::endl;
   fh.writeXYZfile( output, "op.xyz" );
 
